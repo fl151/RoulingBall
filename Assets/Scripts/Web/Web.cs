6d734017134f8452a9b31db5e4947cef4c55ff15
fl151 <118074384+fl151@users.noreply.github.com>
2023-06-16 19:39:42 +0700

    WebUtility

---
+++ Assets/Scripts/Web/Web.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Agava.WebUtility;
+using UnityEngine;
+
+public class Web : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
+    }
+
+    private void OnDisable()
+    {
+        WebApplication.InBackgroundChangeEvent -= OnInBackgroundChange;
+    }
+
+    private void OnInBackgroundChange(bool inBackground)
+    {
+        AudioListener.pause = inBackground;
+        AudioListener.volume = inBackground ? 0f : 1f;
+    }
+}
